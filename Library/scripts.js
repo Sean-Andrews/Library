6cf4467cc8537bc6ca5d1811b81aad881569aa07
Sean Andrews <sandrews2332@gmail.com>
2021-08-31 09:39:18 -0400

    Clean code

--- Library/scripts.js
+++ Library/scripts.js
@@ -1,58 +1,68 @@
-// Initial Query Selectors
+// initial query selectors
 const addBookBtn = document.querySelector('#add-book');
 const inputForm = document.querySelector('.input-form');
 const shelf = document.querySelector('#shelf');
+const cancelBtn = document.querySelector('#cancel');
 
+let myLibrary = JSON.parse(localStorage.getItem('book')) || [];
 
-let myLibrary = [];
+// Book constructor
 
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function() {
-        return `${title} by ${author}, ${pages} long, ${read}.`
-    }
 }
 
-// Function to create card using book information
+function addBookToLibrary(e) {
+    e.preventDefault();
+    const title = this.querySelector(`[name=title]`).value;
+    const author = this.querySelector(`[name=author]`).value;
+    const pages = this.querySelector(`[name=pages]`).value;
+    const read = this.querySelector(`[name=read]`).checked;
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+    createCard(myLibrary, shelf);
+    inputForm.classList.add('hidden');
+    localStorage.setItem('book', JSON.stringify(myLibrary));
+}
 
-function createCard(myLibrary = [], booksList, read) {
-    booksList.innerHTML = myLibrary.map((book, i) => {
-        if (book.read) {
-            return`
-        <div class="card data-book${[i]}">
-            <p>Title: <label for=item${i}>${book.title}</label></p>
-            <p>Author: <label for=item${i}>${book.author}</label></p>
-            <p>Number of Pages: <label for=item${i}>${book.pages}</label></p>
-            <label for=item${i} class="switch">
-            <input for=item${i} id ="unread" type="checkbox" checked>
-            <span for=item${i} class="slider round"></span>
-          </label>
-        </div>
-        `;
-        } else {
-        return`
-        <div class="card data-book${[i]}">
-            <p>Title: <label for=item${i}>${book.title}</label></p>
-            <p>Author: <label for=item${i}>${book.author}</label></p>
-            <p>Number of Pages: <label for=item${i}>${book.pages}</label></p>
-            <label for=item${i} class="switch">
-            <input for=item${i} id ="unread" type="checkbox">
-            <span for=item${i} class="slider round"></span>
-          </label>
-        </div>
-        `;
-        }
+function createCard(myLibrary, bookList) {
+    bookList.innerHTML = myLibrary.map((book, i) => {
+        for (let i = 0; i < myLibrary.length; i++) {
+            if (book.read) {
+                return`
+                <div class="card data-book${[i]}">
+                <p>Title: <label for=item${i}>${book.title}</label></p>
+                <p>Author: <label for=item${i}>${book.author}</label></p>
+                <p>Pages: <label for=item${i}>${book.pages}</label></p>
+                <label for=item${i} class="read-switch">
+                <input for=item${i} class="toggle" type="checkbox" checked>Read<br>
+                <span for=item${i} class="slider round"></span>
+                </label>
+                </div>
+                `;
+            } else {
+                return`
+                <div class="card data-book${[i]}">
+                <p>Title: <label for=item${i}>${book.title}</label></p>
+                <p>Author: <label for=item${i}>${book.author}</label></p>
+                <p>Pages: <label for=item${i}>${book.pages}</label></p>
+                <label for=item${i} class="read-switch">
+                <input for=item${i} class="toggle" type="checkbox">Read<br>
+                <span for=item${i} class="slider round"></span>
+                </label>
+                </div>
+                `;    
+            }
+        }    
     });
-    addDeleteButton();
-    addReadToggle();
+    addDeleteBtn();
+    inputForm.reset();
 }
 
-// Function for delete button
-
-function addDeleteButton() {
+function addDeleteBtn() {
     let eachBook = document.getElementsByClassName('card');
     for (i = 0; i < eachBook.length; i++) {
         const oneBook = eachBook[i];
@@ -71,41 +81,19 @@ function deleteBook(e) {
     createCard(myLibrary, shelf);
 }
 
-// Add Read Toggle Function
-
-function addReadToggle() {
-    for (let i = 0; i < myLibrary.length; i++) {
-        if (myLibrary[i].read) {
-            document.getElementById('unread').checked = true;
-        } else {
-            console.log(myLibrary[i]);
-        }            
-    }
-}
-
-function bookInfo(e) {
-    e.preventDefault();
-    const title = (this.querySelector(`[name=title]`)).value;
-    const author = (this.querySelector(`[name=author]`)).value;
-    const pages = (this.querySelector(`[name=pages]`)).value;
-    const read = ((this.querySelector(`[name=read]`)).checked);
-    const book = new Book(title, author, pages, read);
-    myLibrary.push(book);
-    createCard(myLibrary, shelf, read);
+function cancelInput() {
+    inputForm.reset();
     inputForm.classList.add('hidden');
 }
 
-// Event Listeners
 
 addBookBtn.addEventListener('click', (e) => {
     inputForm.classList.remove('hidden');
-});
+})
 
-inputForm.addEventListener('submit', bookInfo);
+inputForm.addEventListener('submit', addBookToLibrary);
 
+cancelBtn.addEventListener('click', cancelInput);
 
-
-
-
-
+createCard(myLibrary, shelf);
 
